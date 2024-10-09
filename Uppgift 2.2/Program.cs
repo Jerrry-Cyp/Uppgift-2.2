5018d4595988ed11635ddeca49c19254c28af94b
alsa07007 <alsa07007@E-PW05CGR5>
2024-10-09 12:47:57 +0200

    Add project files.

---
+++ Uppgift 2.2/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace Uppgift_2._2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hur långt hoppade Elin i meter?");
+            int hoppadeElin = int.Parse(Console.ReadLine());
+            Console.WriteLine("Hur långt hoppade Alma i meter?");
+            int hoppadeAlma = int.Parse(Console.ReadLine());
+            if (hoppadeElin > hoppadeAlma)
+            {
+                Console.WriteLine("Elin har hoppat " + (hoppadeElin - hoppadeAlma) + " meter längre än Alma.");
+            }
+            if (hoppadeElin < hoppadeAlma)
+            {
+                Console.WriteLine("Alma har hoppat " + (hoppadeAlma - hoppadeElin) + " meter längre än Elin.");
+            }
+        }
+    }
+}
